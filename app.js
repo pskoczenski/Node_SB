544b503f5edc88e874d37ae63a7fbc7221edca12
pskoczenski <pskoczenski@gmail.com>
2017-03-31 19:39:07 -0400

    npm init

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+console.log("Hello Pika")
+
+const sayHi = function() {
+  console.log("Its Friday!")
+};
+
+var counter = function(arr){
+  return 'There are ' +  arr.length  + ' elements in this array'
+};
+
+console.log(counter(["peter", "lauren", "emelina", "olin"]))
+sayHi()
