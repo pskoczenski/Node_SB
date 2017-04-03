5696abafd04be111c192d18941d4838aec4b7b38
pskoczenski <pskoczenski@gmail.com>
2017-04-03 10:10:37 -0400

    add express-validator module

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+// Modules
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
-var logger = function(req, res, next) {
-  console.log('Server Log...');
-  next();
-}
-
-app.use(logger);
-
+// ** MIDDLEWARE **
 // View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -21,15 +17,69 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));
+//Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// ** ROUTES **
+// Root
 app.get('/', function(req, res) {
   var title = "Sandbox";
   res.render('index', {
     title: title
   })
-  //res.send('Hello')
 });
 
+// USERS
+// Post => users/new
+app.post('/users/new', function(req, res){
+
+  req.checkBody('last_name', "First Name is Required").notEmpty();
+  req.checkBody('first_name', "Last Name is Required").notEmpty();
+  req.checkBody('email', "Email is Required").notEmpty();
+  req.checkBody('username', "Username is Required").notEmpty();
+
+  var errors = req.validationErrors();
+
+  if(errors){
+    res.render('index', {
+      errors: errors
+    });
+    console.log("Fields not valid")
+  }else{
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      email: req.body.email
+    }
+    console.log("Success!")
+    console.log(newUser)
+    res.render('users/new')
+
+  };
+
+  })
+// GET => /users/new
+app.get('/users/new', function(req, res){
+  res.render('users/new')
+})
+
+// ** SERVER **
 app.listen(3000, function () {
   console.log('Server fired up on port 3k!')
 });
