fd56d91c45a0fd5a109a768fbcdb0cedec4b37c4
pskoczenski <pskoczenski@gmail.com>
2017-04-04 11:09:26 -0400

    app.js cleanup

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// Modules
+// ** MODULES **
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var expressValidator = require('express-validator');
-var fs = require('fs');
 
 var app = express();
 
