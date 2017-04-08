580d7b17fb852c23e9890b2b19b327bab894cc00
pskoczenski <pskoczenski@gmail.com>
2017-04-08 15:02:48 -0400

    add and delete list items

---
+++ public/assets/todo.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $('.form-row').on('submit', function(){
+
+      var item = $('.form-row input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+          debugger
+        }
+      });
+
+      return false;
+
+  });
+
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+  });
+
+});
